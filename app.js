84bf1646fd18a8f549c2266dd60868a4755d11c1
saluanand <saluanand@gmail.com>
2018-02-12 22:50:51 -0800

    pushing

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 //first parameter is the name
 //second is the dependencies
-var myapp = angular.module('MyApp',[]);
+/*var myapp = angular.module('MyApp',[]);
+//$scope - scope of the controller
 myapp.controller('MyCtrl',['$scope',function($scope){
     $scope.myVal = "Value"
     $scope.onClick = function(){
         $scope.myVal = "New Value"
     }
+}
+]);*/
+
+var myapp = angular.module('MyApp',[]);
+myapp.controller('MyCtrl',['$scope',function($scope){
+    $scope.students = [
+        {
+            name : "ram",
+            age:10
+        },
+        {
+            name : "sam",
+            age : 20
+        }
+    ]
 }
 ]);
